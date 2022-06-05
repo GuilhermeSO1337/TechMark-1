4526b16c5d79b1fc58e9e1924992d2493c0c7504
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-06-04 23:27:45 -0300

    Correções

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -9,11 +9,11 @@ export default function ProfileHeader(){
         <div>
             <div className={styles.Cover}>
                 <div className={styles.Banner}>
-                </div>
                 <div className={styles.BannerIcons}>
                     <Icon icon="akar-icons:whatsapp-fill" className={styles.WhatsappIcon}/>
                     <Icon icon="akar-icons:instagram-fill" className={styles.InstagramIcon} />
                     <Icon icon="akar-icons:facebook-fill" className={styles.FacebookIcon}/>
+                </div>
                 </div>
             </div>
             <div className={styles.ProfileFlexPosition}>
