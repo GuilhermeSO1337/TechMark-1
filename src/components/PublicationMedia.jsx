8f81f069540a4e44c2e654adb2e19d18d0c18ee7
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-06-18 20:04:43 -0300

    Media verification fixed

--- src/components/PublicationMedia.jsx
+++ src/components/PublicationMedia.jsx
@@ -1,14 +1,13 @@
 export default function PublicationMedia(PublicationData){
-    let Dir = JSON.stringify(PublicationData.media)
-    let Type = Dir.match('jpg');
     let MediaElement;
-    switch (Type) {
-        case 'jpg':
-            MediaElement = <img src='' alt=''/>
+    console.log(PublicationData)
+    switch (PublicationData.MediaType) {
+        case '.jpg':
+            MediaElement = <img src={PublicationData.MediaDir} alt=''/>
             break;
-        case 'mp4':
+        case '.mp4':
             MediaElement = <video controls autoPlay={true} >
-                <source src='' type="video/mp4" />
+                <source src={PublicationData.MediaDir} type="video/mp4" />
             </video>
             break;
     
--- src/components/PublicationSection.jsx
+++ src/components/PublicationSection.jsx
@@ -6,17 +6,20 @@ import image from './../img/Limpeza.jpg';
 let publication= [{
     ProfilePicture:'static/media/Profile_Picture.2a28de9cc240c3058614.jpg',
     Tittle:'Alteração no horário',
-    Description:'Nesta segunda-feira (23/05) estaremos abertos somente até as 18h.'
+    Description:'Nesta segunda-feira (23/05) estaremos abertos somente até as 18h.',
+    MediaType:null
 },{
 ProfilePicture:'static/media/Profile_Picture.2a28de9cc240c3058614.jpg',
 Tittle:'Limpe seu computador:',
 Description:'Você sabia que a limpeza de seu computador pode prolongar a vida útil do equipamento? ',
-media: '/static/media/video01.015421ae62da53f1d323.mp4',
+MediaDir: '/static/media/video01.015421ae62da53f1d323.mp4',
+MediaType:'.mp4'
 },{
     ProfilePicture:'static/media/Profile_Picture.2a28de9cc240c3058614.jpg',
     Tittle:'Montamos o seu computador:',
     Description:'Você sabia que a limpeza de seu computador pode prolongar a vida útil do equipamento? ',
-    media: '/static/media/Limpeza.694a184963c97380e6b6.jpg',
+    MediaDir: '/static/media/Limpeza.694a184963c97380e6b6.jpg',
+    MediaType:'.jpg'
     }
 ]
 export default function PublicationSection(){
