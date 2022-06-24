8df400d748f6e9940db0a0bcba10154c403b7812
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-06-24 11:34:17 -0300

    Commenter filter OK

---
+++ src/components/ClientComment.jsx
@@ -0,0 +1,9 @@
+import styles from "./../css/Comment.module.css";
+
+export default function ClientComment(){
+    return(
+        <div className={styles.Comment}>
+            <p>asasasas</p>
+        </div>
+    );
+}
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,5 +1,6 @@
 import styles from "./../css/CommentSection.module.css";
 import { Icon } from '@iconify/react';
+import ClientComment from "./ClientComment";
 
 export default function CommentSection(){
     return(
@@ -8,9 +9,17 @@ export default function CommentSection(){
                 <Icon className={styles.ProfilePicture} icon="carbon:user-avatar-filled-alt"/>
                 <input className={styles.CommentField} type="text" name="Coment"/>
                 <Icon className={styles.SendComment} icon="fluent:send-16-filled" />
-                <hr/>
         </form>
-        <div className={styles.CommentsArea} >
+        <div className={styles.CommentsArea}>
+            <hr/>
+            <div className={styles.CommentFilter}>
+                <div className={styles.RateFilter}>
+                    <p>Avaliações(1208)</p>
+                    <Icon icon="akar-icons:chevron-down" width="30px" color="#525252" cursor="pointer" />
+                </div>
+                <Icon icon="bi:filter-right" width="40px" color="#525252" cursor="pointer"/>
+            </div>
+            <ClientComment/>
         </div>
     </section>
     )
